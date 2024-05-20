31576b9a34d5e2813ea862a861caaf2eab826f93
tbase <laelli@wesure.cn>
2024-05-20 09:02:27 +0800

    fix: add revalidatePath

--- src/actions/topic.ts
+++ src/actions/topic.ts
@@ -3,6 +3,7 @@
 import * as zfd from '~/lib/zod-form-data'
 import { mustAuth } from './auth'
 import { createThread, getUserTopics } from './common'
+import { revalidatePath } from 'next/cache'
 
 export const createTopicThread = async (topicId: number, formData: FormData) => {
   const session = await mustAuth()
@@ -12,13 +13,14 @@ export const createTopicThread = async (topicId: number, formData: FormData) => 
     })
     .parse(formData)
 
-  return await createThread({
+  await createThread({
     topic_id: topicId,
     thread_content: form.thread_content,
     color: 'None',
     updated_at: new Date(),
     user_id: session.userId,
   })
+  revalidatePath(`/topics/${topicId}`)
 }
 
 export const getTopicList = async () => {
