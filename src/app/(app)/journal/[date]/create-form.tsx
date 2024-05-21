a3751a43222ede50fc567c7b54f0e490c39a4c33
tbase <laelli@wesure.cn>
2024-05-21 19:06:50 +0800

    fix: await

--- src/app/(app)/journal/[date]/create-form.tsx
+++ src/app/(app)/journal/[date]/create-form.tsx
@@ -5,7 +5,7 @@ import { createJournalThread } from '~/actions/journal'
 
 export const CreateForm = ({ date }: { date: string }) => {
   const createJournalThread_ = async (formData: FormData) => {
-    createJournalThread(date, formData)
+    await createJournalThread(date, formData)
   }
   return <ThreadFormCreate onSubmit={createJournalThread_} />
 }
