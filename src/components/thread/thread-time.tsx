6137158b8515c31d66c97492eb2dcfe649d4a1f7
tbase <laelli@wesure.cn>
2024-05-24 17:16:24 +0800

    fix: toLocaleString

--- src/components/thread/thread-time.tsx
+++ src/components/thread/thread-time.tsx
@@ -1,7 +1,7 @@
 import { formatDate } from 'date-fns'
 
 export default function ThreadTime({ time, format }: { time: Date; format: string }) {
-  const timeString = time.toISOString()
+  const timeString = time.toLocaleString()
   return (
     <time dateTime={timeString} suppressHydrationWarning>
       {formatDate(time, format)}
